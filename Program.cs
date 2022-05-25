bd973f0da0d37717a924b363da75e23245551114
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-25 10:34:20 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ArraysV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var  numbers = new [] { 3, 7, 2, 6, 12, 69 };
+
+            //lenght
+            Console.WriteLine("Length:" + numbers.Length);
+        }
+    }
+}
